7d0bf1cf6a4810c9d6a7610284adc7206aae3108
12YBoke <yvesboke84@gmail.com>
2025-04-16 13:20:54 +0100

    Ajout du rôle lors de la création d'un utilisateur dans la fonction POST.

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,20 +1,18 @@
-import prisma from '@/lib/prisma';
+import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function POST (
-  req: Request
-) {
-  const { name, extensionid, hash, salt } = await req.json();
+export async function POST(req: Request) {
+  const { name, extensionid, hash, salt, role } = await req.json();
 
   await prisma.user.create({
-    data: { 
-      name : name, 
-      extensionId : extensionid, 
-      password : hash,
-      salt : salt,
+    data: {
+      name: name,
+      extensionId: extensionid,
+      password: hash,
+      salt: salt,
+      role: role,
     },
   });
 
   return NextResponse.json({ status: 200 });
-
 }
